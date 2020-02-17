ea1b144d0ee51f0d66bf4d4154f0fbcfdb726a02
Adeyemi Ayodipe <ayodipeadeyemi@gmail.com>
2020-02-16 22:39:52 -0600

    Fetching data from NewYorkTime API

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// require('dotenv').config()
 const divSelector = (e) => {
     return document.querySelector(e)
 }
@@ -9,13 +10,25 @@ const endYear = divSelector('#end-year')
 const search = divSelector('.search')
 const clear = divSelector('.clear')
 const results = divSelector('.articles')
+const key = config.key
+
 let articleName, num, sYear, eYear
 
 
 search.addEventListener('click', (e) => {
+
     e.preventDefault();
     articleName = searchTerm.value.trim();
     num = numberOfRecords.options[numberOfRecords.selectedIndex].value;
     sYear = parseInt(startYear.value);
     eYear = parseInt(endYear.value)
+    let URL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + articleName + '& begin_date = ' + sYear + '& end_date = ' +
+        eYear + ' & api - key =' + key + ''
+
+    fetch(URL.replace(/\s/g, '')).then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+            console.log(myJson);
+        });
 })
